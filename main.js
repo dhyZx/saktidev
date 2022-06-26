81e236e4076a0ed1760470b4c897bede47860f1a
Adhy <76676115+dhyZx@users.noreply.github.com>
2022-06-26 11:40:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-jQuery(document).ready(function() {
-  jQuery('.skillbar').each(function() {
-    jQuery(this).find('.skillbar-bar').animate({
-      width: jQuery(this).attr('data-percent')
-    }, 4000);
-  });
-});
 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
